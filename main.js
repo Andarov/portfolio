d541984127f4f507c864a20a9dc66f22ad8b0717
Andarov <bekturandarob678@gmail.com>
2022-06-16 08:55:22 +0500

    Add dark-mode ecommerce.html

--- main.js
+++ main.js
@@ -1,10 +1,22 @@
-const elDay = document.querySelector(".input-day");
-const elNight = document.querySelector(".input-night");
+const elsDay = document.querySelectorAll(".input-day");
+const elsNight = document.querySelectorAll(".input-night");
 
-elDay.addEventListener("click", function () {
-    document.body.classList.remove("dark-mode");
-});
+// elDay.addEventListener("click", function () {
+//     document.body.classList.remove("dark-mode");
+// });
 
-elNight.addEventListener("click", function () {
-    document.body.classList.add("dark-mode");
-});
+// elNight.addEventListener("click", function () {
+//     document.body.classList.add("dark-mode");
+// });
+
+elsDay.forEach(function(item,index) {
+    item.addEventListener("click", function () {
+        document.body.classList.remove("dark-mode");
+    })
+})
+
+elsNight.forEach(function(item,index) {
+    item.addEventListener("click", function () {
+        document.body.classList.add("dark-mode");
+    })
+})
