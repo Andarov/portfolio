6948c7dd4ed1a87f13f8e6bd57de6eb4278b7297
Andarov <bekturandarob678@gmail.com>
2022-11-01 15:02:51 +0500

    Change day and night mode styles and set localstorage

--- main.js
+++ main.js
@@ -1,18 +1,37 @@
-const elsDay = document.querySelectorAll(".input-day");
-const elsNight = document.querySelectorAll(".input-night");
+const elsDay = document.querySelectorAll(".btn--day");
+const elsNight = document.querySelectorAll(".btn--night");
+
+const modeLocal = localStorage.getItem('mode')
+
+if(modeLocal){
+    document.body.classList.add("dark-mode");
+    elsDay.forEach(function(item){
+        item.classList.toggle('visually-hidden')
+    })
+    elsNight.forEach(function(item){
+        item.classList.toggle('visually-hidden')
+    })
+}
 
 const toggleMode = function(){
     document.body.classList.toggle("dark-mode");
+    elsDay.forEach(function(item){
+        item.classList.toggle('visually-hidden')
+    })
+    elsNight.forEach(function(item){
+        item.classList.toggle('visually-hidden')
+    })
 }
-
 elsDay.forEach(function(item,index) {
     item.addEventListener("click", function () {
         toggleMode()
+        localStorage.setItem('mode', '')
     })
 })
 
 elsNight.forEach(function(item,index) {
     item.addEventListener("click", function () {
         toggleMode()
+        localStorage.setItem('mode', 'dark-mode')
     })
 })
